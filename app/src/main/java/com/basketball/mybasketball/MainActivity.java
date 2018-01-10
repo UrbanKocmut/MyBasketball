c903cff6bd93b69b0f02e1debbd08731ad020402
UrbanKocmut <u.kocmut@gmail.com>
2018-01-10 12:33:21 +0100

    Dodal shranjevanje imena lastnika preko Shared preferences, EZ CYKA

--- app/src/main/java/com/basketball/mybasketball/MainActivity.java
+++ app/src/main/java/com/basketball/mybasketball/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.arch.persistence.room.Room;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,12 +16,18 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
     TextView name;
-
+    String owner;
+    public static final String PREFS_NAME = "MY_NAME";
+    public static final String PREFS_KEY = "owner";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        owner = settings.getString(PREFS_KEY, "by Urban Kocmut");
+
 
         setContentView(R.layout.activity_main);
 
@@ -33,6 +40,7 @@ public class MainActivity extends Activity {
         });
 
         name = (TextView) findViewById(R.id.appOwner);
+        name.setText(owner);
 
         final Button Tekma = findViewById(R.id.TekmaBtn);
         Tekma.setOnClickListener(new View.OnClickListener() {
@@ -53,9 +61,23 @@ public class MainActivity extends Activity {
 
     }
 
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        // We need an Editor object to make preference changes.
+        // All objects are from android.context.Context
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(PREFS_KEY, owner);
+
+        // Commit the edits!
+        editor.apply();
+    }
+
     void changeOwner(){
         Intent i = new Intent(this, ChangeName.class);
-        i.putExtra("NAME",name.getText().toString());
+        i.putExtra("NAME",owner);
         startActivityForResult(i, 1);
     }
 
@@ -64,6 +86,7 @@ public class MainActivity extends Activity {
         if (requestCode == 1) {
             if(resultCode == RESULT_OK) {
                 String strName = data.getStringExtra("NAME");
+                owner = strName;
                 name.setText(strName);
             }
         }
